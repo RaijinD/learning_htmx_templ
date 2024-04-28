a302b4b76582c989b0d55ac95ff73abc329a49e6
RaijinD <edecarlosa.16@gmail.com>
2024-04-28 19:27:18 -0300

    htmx and templ working... i think...

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module htmx_templ
 
 go 1.22.0
+
+require github.com/a-h/templ v0.2.598
--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	Components "htmx_templ/view"
+	"net/http"
 
-func main(){
-  fmt.Println("Hello World!!!")
+	"github.com/a-h/templ"
+)
+
+func main() {
+	component := Components.Hello("Raijin")
+
+	http.Handle("/", templ.Handler(component))
+	http.HandleFunc("/clicked", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+			Components.Home().Render(r.Context(), w)
+			return
+		}
+
+	})
+
+	fmt.Printf("Listening on :3000")
+
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 }
